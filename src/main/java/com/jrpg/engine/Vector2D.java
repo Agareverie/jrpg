508ac0aa9d3981c286da3472b0a568d03d1d7232
konomiyuzu <autophukla@gmail.com>
2025-04-15 13:47:17 +0700

    implemented a Vector class

---
+++ src/main/java/com/jrpg/engine/Vector2D.java
@@ -0,0 +1,71 @@
+package com.jrpg.engine;
+
+import com.jrpg.rendering.Coordinate;
+
+public class Vector2D {
+    private double x;
+    private double y;
+
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    static Vector2D zero(){
+        return new Vector2D(0, 0);
+    }
+
+    public Vector2D(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector2D add(double x, double y){
+        return new Vector2D(this.x + x, this.y + y);
+    }
+
+    public Vector2D add(Vector2D other){
+        return this.add(other.getX(), other.getY());
+    }
+
+    public Vector2D sub(double x, double y){
+        return new Vector2D(this.x - x, this.y - y);
+    }
+
+    public Vector2D sub(Vector2D other){
+        return this.sub(other.getX(), other.getY());
+    }
+
+    public Vector2D scale(double x, double y){
+        return new Vector2D(this.x * x, this.y * y);
+    }
+
+    public Vector2D scale(double c){
+        return this.scale(c,c);
+    }
+
+    public Vector2D scale(Vector2D scalingVector){
+        return this.scale(scalingVector.getX(), scalingVector.getY());
+    }
+
+    public double getLength(){
+        return Math.sqrt(x*x + y*y);
+    }
+
+    /**
+     * toCoordinate
+     * @return Coordinate instance with x and y rounded from the Vector's x and y
+     */
+    public Coordinate toCoordinate(){
+        return new Coordinate((int) Math.round(x), (int) Math.round(y));
+    }
+}
+
