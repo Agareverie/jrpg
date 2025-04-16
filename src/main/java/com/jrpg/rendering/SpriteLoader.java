e229226913bfdabb8400f18db56bbe0ebb618a18
konomiyuzu <autophukla@gmail.com>
2025-04-16 10:04:19 +0700

    implemented SpriteLoader refactor

--- src/main/java/com/jrpg/rendering/SpriteLoader.java
+++ src/main/java/com/jrpg/rendering/SpriteLoader.java
@@ -8,43 +8,42 @@ import java.util.stream.Stream;
 
 import javax.imageio.ImageIO;
 
-//TODO: switch this to a singleton
 public class SpriteLoader {
-    
-    private static boolean initialized = false;
-    private static HashMap<String, Image> sprites;
-
-    //maybe make this mofifiable at runtime?
-    //for now it's constant from startup
     private static String defaultSpriteName = "default";
+    private HashMap<String, Image> sprites;
 
-    private static void initialize(){
+    private static final class SingletonHolder{
+        public static final SpriteLoader INSTANCE = new SpriteLoader();
+    }
+
+    public static SpriteLoader getInstance(){
+        return SingletonHolder.INSTANCE;
+    }
+
+    private SpriteLoader(){
         sprites = new HashMap<String, Image>();
 
-        URL url = SpriteLoader.class.getResource("/sprites");
-        
-        Stream<Path> paths;
-        try {
-            paths = Files.walk(Path.of(url.toURI()));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        Stream<Path> paths = loadSpritesFolder();
 
         paths.filter(Files::isRegularFile).forEach(path -> {
             String fileName = path.getFileName().toString();
 
-            //don't name our files with more than 1 .
-            //(or modify this piece of code to support that first)
-            String spriteName = fileName.split("\\.")[0];
+            String[] spriteNameParts = fileName.split("\\.");
+            StringJoiner spriteNameJoiner = new StringJoiner(".");
+
+            for (int i = 0; i < spriteNameParts.length - 1; i++) {
+                spriteNameJoiner.add(spriteNameParts[i]);
+            }
+
+            String spriteName = spriteNameJoiner.toString();
 
             Image sprite;
             try {
                 sprite = ImageIO.read(path.toFile());
+                sprites.put(spriteName, sprite);
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-
-            sprites.put(spriteName, sprite);
         });
 
         paths.close();
@@ -52,14 +51,34 @@ public class SpriteLoader {
         if(!sprites.containsKey(defaultSpriteName)){
             throw new RuntimeException("no default sprite with name " + defaultSpriteName + " found");
         }
-
     }
 
-    public static Image getSprite(String name){
-        if(!SpriteLoader.initialized) SpriteLoader.initialize();
+    public static Image getSprite(String name) {
+        HashMap<String, Image> sprites = getInstance().sprites;
 
-        if(sprites.containsKey(name)) return sprites.get(name);
-        else return sprites.get(defaultSpriteName);
+        if (sprites.containsKey(name)) {
+            return sprites.get(name);
+        }
+
+        return sprites.get(defaultSpriteName);
+    }
+
+    private static Stream<Path> loadSpritesFolder(){
+        URL spritesUrl = SpriteLoader.class.getResource("/sprites");
+
+        try {
+            return Files.walk(Path.of(spritesUrl.toURI()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setDefaultSpriteName(String defaultSpriteName) {
+        if (!getInstance().sprites.containsKey(defaultSpriteName)) {
+            throw new RuntimeException("no default sprite with name " + defaultSpriteName + " found");
+        }
+
+        SpriteLoader.defaultSpriteName = defaultSpriteName;
     }
     
 }
