d6f73e6b304493267eb4f1cb6691d17ad6d84747
konomiyuzu <autophukla@gmail.com>
2025-04-18 17:21:26 +0700

    removed unused paused state

--- src/main/java/com/jrpg/engine/GameState.java
+++ src/main/java/com/jrpg/engine/GameState.java
@@ -5,7 +5,6 @@ package com.jrpg.engine;
 public class GameState {
     private Engine engine;
     private boolean inActionMenu = false;
-    private boolean paused = false;
 
     public boolean isInActionMenu() {
         return inActionMenu;
@@ -13,14 +12,9 @@ public class GameState {
     public boolean isInDialogue() {
         return engine.getCurrentDialogue() != null;
     }
-    public boolean isPaused() {
-        return paused;
-    }
+
     public void setInActionMenu(boolean inActionMenu) {
         this.inActionMenu = inActionMenu;
-    }
-    public void setPaused(boolean paused) {
-        this.paused = paused;
     }
 
     public GameState(Engine engine){
