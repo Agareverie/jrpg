6b87b61836ce6341d56765db78eb000be9509632
konomiyuzu <autophukla@gmail.com>
2025-04-22 00:07:11 +0700

    removed unused code

--- src/main/java/com/jrpg/example_game/Player.java
+++ src/main/java/com/jrpg/example_game/Player.java
@@ -3,16 +3,6 @@ package com.jrpg.example_game;
 import com.jrpg.engine.components.Dialogue;
 
 public class Player extends GameCharacter {
-    private int money = 0;
-
-    public int getMoney() {
-        return money;
-    }
-
-    public void setMoney(int money) {
-        this.money = money;
-    }
-
     public Player(int health, GameStats baseStats){
         super("player", health, baseStats);
         setDescription(Dialogue.fromString("You"));
